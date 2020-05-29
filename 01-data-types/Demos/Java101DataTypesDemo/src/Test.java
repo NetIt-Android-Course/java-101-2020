512b43b6551db951d7451f209a2d68180192f770
Teodor Kostadinov <teodor.kk.93@gmail.com>
2020-05-29 17:23:59 +0300

    Add demo files for data types lecture

---
+++ 01-data-types/Demos/Java101DataTypesDemo/src/Test.java
@@ -0,0 +1,64 @@
+public class Test {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello");
+//        System.out.println("World");
+//
+//        String name = "Ivan";
+//        String name2;
+//
+//        System.out.println(name);
+//        name2 = "George";
+//        System.out.println(name2);
+//        name2 = "Teo";
+//        System.out.println(name2);
+
+//        System.out.println("     ^");
+//        System.out.println("    / \\");
+//        System.out.println("   /   \\");
+//        System.out.println("  /     \\");
+//        System.out.println(" /       \\");
+//        System.out.println("/         \\");
+//        System.out.println("-------------");
+
+        String favouriteBook = "My \nfavourite \nbook is \"Twil\night\".";
+        System.out.println(favouriteBook);
+
+        float distance = 3.14444f;
+
+        byte age = 100;
+
+        char letterF = 'F';
+        char letterB = 'B';
+        System.out.println((char) 65);
+
+        int money = 1121212;
+        long money2 = money;
+
+        double doubleTrouble = money;
+        float floatTrouble = (float) doubleTrouble;
+
+        System.out.println(floatTrouble);
+
+        //Задача 1
+        String firstName;
+        String secondName;
+        String thirdName;
+        float moneyBalance;
+        String bankName;
+        String bic;
+        String iban;
+        String creditCardNumber;
+        short ccv;
+
+        //Задача 2
+        String employeeFirstName;
+        String employeeFamilyName;
+        byte employeeAge;
+        char sex;
+        String idCardNumber;
+        String employeeCompanyNumber;
+        boolean isMarried;
+
+    }
+}
